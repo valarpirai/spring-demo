5227063e5cd3a0b72efb3bd0cce67feeefb8fa9b
Valarpiraichandran <valarpiraichandran@Valarpiraichandrans-MacBook-Pro.local>
2024-06-25 21:40:47 +0530

    Initial Commit with default downloaded code

---
+++ src/main/kotlin/com/example/demo/HomeController.kt
@@ -0,0 +1,12 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController class HomeController {
+
+    @GetMapping("/")
+    fun home() : String {
+        return "Hello World";
+    }
+}
