f643170808f2742b3024331188b71a7e18333fac
Valar <a.valarpirai@gmail.com>
2025-04-25 15:24:56 +0530

    Setup logging

--- src/main/java/com/example/demo/controller/HealthController.java
+++ src/main/java/com/example/demo/controller/HealthController.java
@@ -1,13 +1,31 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HealthController {
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @RequestMapping(value = "/" )
+  Map<String, String> index(){
+    logger.trace("This is a trace message");
+    logger.debug("This is a debug message");
+    logger.info("This is an info message");
+    logger.warn("This is a warn message");
+    logger.error("This is an error message");
+
+    return Map.of("message", "Hello, World!");
+  }
 
   @GetMapping("/health")
   String health() {
+    logger.info("Health path");
     return "Ok";
   }
 }
