4b8e6eafe9b45e3aeabdeb0f15d6b26fbda93ce4
Valar <a.valarpirai@gmail.com>
2025-05-12 23:12:23 +0530

    Remove unwanted config

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.example.demo.model")
 public class DemoApplication {
 
 	public static void main(String[] args) {
