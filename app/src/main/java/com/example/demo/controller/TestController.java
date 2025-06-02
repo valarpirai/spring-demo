9ca0591a6dc62e5b12994cd259d3fb2e49cf5d46
Valar <a.valarpirai@gmail.com>
2025-06-02 11:27:58 +0530

    Trigger OOM error for debugging

--- app/src/main/java/com/example/demo/controller/TestController.java
+++ app/src/main/java/com/example/demo/controller/TestController.java
@@ -1,15 +1,22 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private List<byte[]> memoryHog = new ArrayList<>();
 
     @Autowired
     private BookService bookService;
@@ -21,6 +28,19 @@ public class TestController {
             bookService.findAllBooksWithReviews(0, 10);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        }
+        return "Done";
+    }
+
+    @GetMapping("/trigger-oom")
+    String outOfMemoryTest() {
+        try {
+            while (true) {
+                memoryHog.add(new byte[1024 * 1024]); // 1MB per object
+                logger.info("Allocated {} MB", memoryHog.size());
+            }
+        } catch (OutOfMemoryError e) {
+            logger.error("Caught OutOfMemoryError: {}, List size: {}", e.getMessage(), memoryHog.size());
         }
         return "Done";
     }
